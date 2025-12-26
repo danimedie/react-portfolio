0885573aa10f531fc4025d5997c5e702cb370b90
medié <danimedie@gmail.com>
2025-12-26 01:01:24 +0100

    setup header and footer

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,5 +1,7 @@
 export function Footer () {
   return (
-    <h1>Footer</h1>
+    <footer className='flex justify-center p-2 text-purple-400 opacity-50 text-sm text-shadow-black text-shadow-md'>
+      &copy; Dani Medié. All rights reserved.
+    </footer>
   )
 }
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,5 +1,66 @@
+import { Link } from 'react-router-dom'
+import mediemoji from '../../assets/mediemoji.png'
+import { FiGithub } from 'react-icons/fi'
+
+const NavItem = ({ link, children }) => {
+  const isExternal = link.startsWith('http')
+  const sharedStyles = 'hover:bg-purple-400/40 rounded-full transition-all duration-400'
+
+  return (
+    <li>
+      {isExternal
+        ? (
+          <a
+            href={link}
+            target='_blank'
+            rel='noopener noreferrer'
+            className={`${sharedStyles}`}
+          >
+            {children}
+          </a>
+          )
+        : (
+          <Link
+            to={link}
+            className={`${sharedStyles} `}
+          >
+            {children}
+          </Link>
+          )}
+    </li>
+  )
+}
+
 export function Header () {
   return (
-    <h1>Header</h1>
+    <header>
+      <nav className='p-2'>
+        <ul className='flex justify-center items-center gap-5'>
+          <NavItem link='/'>
+            <img
+              src={mediemoji}
+              alt='Emoji de Dani Medié sonriendo'
+              className='w-10 h-10 object-contain'
+            />
+          </NavItem>
+
+          <NavItem link='/experience'>
+            Experience
+          </NavItem>
+
+          <NavItem link='/projects'>
+            Projects
+          </NavItem>
+
+          <NavItem link='/About'>
+            About
+          </NavItem>
+
+          <NavItem link='https://github.com/danimedie'>
+            <FiGithub className='w-6 h-6' />
+          </NavItem>
+        </ul>
+      </nav>
+    </header>
   )
 }
